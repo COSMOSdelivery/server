f0fe1fccc9058ac3e91cd2905042bf1a4f94536d
Iheb-AG <ihebagrebi810@gmail.com>
2024-11-11 04:28:48 +0000

    updated register and login logic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require('dotenv/config')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const authJwt = require("./middleware/expressjwt");
+const authJwt = require("./src/middleware/expressjwt");
 const usersRouter = require("./src/models/user")
 
 const app = express()
---
+++ src/generated/prisma-client/index.ts
@@ -0,0 +1,7 @@
+import { Utilisateur as _Utilisateur } from './utilisateur'
+
+export namespace PrismaModel {
+	export class Utilisateur extends _Utilisateur {}
+
+	export const extraModels = [Utilisateur]
+}
